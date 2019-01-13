2c8b8e1dc484d0597e09b69177b1176bb8be0595
kevinli23 <cskevinli23@gmail.com>
2019-01-13 18:08:58 -0500

    changes to navbar

--- scripts/script.js
+++ scripts/script.js
@@ -13,6 +13,48 @@ var color = [
 
 var index = 0;
 
+var descriptions = {
+  "chronovise-card":
+    "<h3 style='text-align: left'>Chronovise</h3>" +
+    "<h5 style='color:gray'>Programmed in: HTML CSS JavaScript jQuery Chrome API</h5>" +
+    "<h5 style='color:gray'>September 2018</h5>" +
+    "<h5>&emsp;• Worked in a team to create a Google Chrome extension that stored multiple links into a workspace, and upon press, all links would be automatically opened in new tabs</h5>" +
+    "<h5>&emsp;• I focused heavily on the JavaScript and Chrome API functionality, namely the functions that controlled the opening of tabs and manipulation of saved data</h5>",
+  "pengu-card":
+    "<h3 style='text-align: left'>Pengu's Dank Adventure</h3>" +
+    "<h5 style='color:gray'>Programmed in: Java</h5>" +
+    "<h5 style='color:gray'>September 2017 - Present</h5>" +
+    "<h5>&emsp;• Platformer game that used Java's Graphics class for rendering and a time based loop for its updates</h5>" +
+    "<h5>&emsp;• Applies the core concepts of Object Oriented Programming</h5>" +
+    "<h5>&emsp;• Has a Java programmed server that can handle logins using sockets</h5>",
+  "portfolio-card":
+    "<h3 style='text-align: left'>My Portfolio Website</h3>" +
+    "<h5 style='color:gray'>Programmed in: HTML CSS JavaScript jQuery Bootstrap</h5>" +
+    "<h5 style='color:gray'>January 2019</h5>" +
+    "<h5>&emsp;• Portfolio website that showcases a majority of my personal projects</h5>" +
+    "<h5>&emsp;• Tells a little bit more about myself as well as ways to get in contact with me</h5>",
+  "darkrun-card":
+    "<h3 style='text-align: left'>Dark Run</h3>" +
+    "<h5 style='color:gray'>Programmed in: Java</h5>" +
+    "<h5 style='color:gray'>January 2018 - Feburary 2018</h5>" +
+    "<h5>&emsp;• Maze platformer with limitted light</h5>" +
+    "<h5>&emsp;• Applies the core concepts of Object Oriented Programming</h5>" +
+    "<h5>&emsp;• Collision detection is done by entities in a given radius rather than checking all entities</h5>",
+  "pomodoro-card":
+    "<h3 style='text-align: left'>Pomodoro Timer</h3>" +
+    "<h5 style='color:gray'>Programmed in: Python</h5>" +
+    "<h5 style='color:gray'>October 2018 - Present</h5>" +
+    "<h5>&emsp;• Timer that alternates between a 30 - 5 minute timer</h5>" +
+    "<h5>&emsp;• Also plays music from a specific directory of the program</h5>" +
+    "<h5>&emsp;• GUI and music is done through pygame while the timer is done through a Python thread</h5>",
+  "timetracker-card":
+    "<h3 style='text-align: left'>Time Usage Tracker</h3>" +
+    "<h5 style='color:gray'>Programmed in: HTML CSS JavaScript jQuery Chrome API</h5>" +
+    "<h5 style='color:gray'>December 2018 - Present</h5>" +
+    "<h5>&emsp;•	Collaborated with a peer to create a Google Chrome extension that tracks time spent on each website per day</h5>" +
+    "<h5>&emsp;•	Simply HTML design with more of a focus towards the JavaScript and jQuery backend functionality"
+};
+
 $(window).scroll(function() {
   $("nav").toggleClass("scrolled", $(this).scrollTop() > 100);
 });
@@ -36,81 +78,18 @@ $(document).ready(function() {
     $("#portfolio").css("color", color[index]);
   });
 
-  $("div#chronovise-card").hover(function() {
-    $("span.project-title").append(
-      "<h3 style='text-align: left'>Chronovise</h3>" +
-        "<h5 style='color:gray'>Programmed in: HTML CSS JavaScript jQuery Chrome API</h5>" +
-        "<h5 style='color:gray'>September 2018</h5>" +
-        "<h5>&emsp;• Worked in a team to create a Google Chrome extension that stored multiple links into a workspace, and upon press, all links would be automatically opened in new tabs</h5>" +
-        "<h5>&emsp;• I focused heavily on the JavaScript and Chrome API functionality, namely the functions that controlled the opening of tabs and manipulation of saved data</h5>"
-    );
+  $(
+    "div#chronovise-card, div#pengu-card, div#portfolio-card, div#darkrun-card, div#pomodoro-card, div#timetracker-card"
+  ).hover(function() {
+    $("span.project-title").empty();
+    $("span.project-title").append(descriptions[this.id]);
     $("span.project-title").fadeIn(500);
   });
 
   $(
     "div#chronovise-card, div#pengu-card, div#portfolio-card, div#darkrun-card, div#pomodoro-card, div#timetracker-card"
   ).mouseleave(function() {
-    $("span.project-title").empty();
     $("span.project-title").css("display", "none");
-  });
-
-  $("div#pengu-card").hover(function() {
-    $("span.project-title").append(
-      "<h3 style='text-align: left'>Pengu's Dank Adventure</h3>" +
-        "<h5 style='color:gray'>Programmed in: Java</h5>" +
-        "<h5 style='color:gray'>September 2017 - Present</h5>" +
-        "<h5>&emsp;• Platformer game that used Java's Graphics class for rendering and a time based loop for its updates</h5>" +
-        "<h5>&emsp;• Applies the core concepts of Object Oriented Programming</h5>" +
-        "<h5>&emsp;• Has a Java programmed server that can handle logins using sockets</h5>"
-    );
-    $("span.project-title").fadeIn(500);
-  });
-
-  $("div#portfolio-card").hover(function() {
-    $("span.project-title").append(
-      "<h3 style='text-align: left'>My Portfolio Website</h3>" +
-        "<h5 style='color:gray'>Programmed in: HTML CSS JavaScript jQuery Bootstrap</h5>" +
-        "<h5 style='color:gray'>January 2019</h5>" +
-        "<h5>&emsp;• Portfolio website that showcases a majority of my personal projects</h5>" +
-        "<h5>&emsp;• Tells a little bit more about myself as well as ways to get in contact with me</h5>"
-    );
-
-    $("span.project-title").fadeIn(500);
-  });
-
-  $("div#darkrun-card").hover(function() {
-    $("span.project-title").append(
-      "<h3 style='text-align: left'>Dark Run</h3>" +
-        "<h5 style='color:gray'>Programmed in: Java</h5>" +
-        "<h5 style='color:gray'>January 2018 - Feburary 2018</h5>" +
-        "<h5>&emsp;• Maze platformer with limitted light</h5>" +
-        "<h5>&emsp;• Applies the core concepts of Object Oriented Programming</h5>" +
-        "<h5>&emsp;• Collision detection is done by entities in a given radius rather than checking all entities</h5>"
-    );
-    $("span.project-title").fadeIn(500);
-  });
-
-  $("div#pomodoro-card").hover(function() {
-    $("span.project-title").append(
-      "<h3 style='text-align: left'>Pomodoro Timer</h3>" +
-        "<h5 style='color:gray'>Programmed in: Python</h5>" +
-        "<h5 style='color:gray'>October 2018 - Present</h5>" +
-        "<h5>&emsp;• Timer that alternates between a 30 - 5 minute timer</h5>" +
-        "<h5>&emsp;• Also plays music from a specific directory of the program</h5>" +
-        "<h5>&emsp;• GUI and music is done through pygame while the timer is done through a Python thread</h5>"
-    );
-    $("span.project-title").fadeIn(500);
-  });
-
-  $("div#timetracker-card").hover(function() {
-    $("span.project-title").append(
-      "<h3 style='text-align: left'>Time Usage Tracker</h3>" +
-        "<h5 style='color:gray'>Programmed in: HTML CSS JavaScript jQuery Chrome API</h5>" +
-        "<h5 style='color:gray'>December 2018 - Present</h5>" +
-        "<h5>&emsp;•	Collaborated with a peer to create a Google Chrome extension that tracks time spent on each website per day</h5>" +
-        "<h5>&emsp;•	Simply HTML design with more of a focus towards the JavaScript and jQuery backend functionality"
-    );
-    $("span.project-title").fadeIn(500);
   });
 
   $(".section-link").click(function() {
